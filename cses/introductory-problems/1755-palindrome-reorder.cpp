ae622560168f396e828ae9f6b91205b03e6f9f74
ManukMelkonyan <myan.manuk@gmail.com>
2025-08-13 12:12:00 +0400

    fix(cses): simplified the solution for `Palindrome Reorder: 1755`

--- cses/introductory-problems/1755-palindrome-reorder.cpp
+++ cses/introductory-problems/1755-palindrome-reorder.cpp
@@ -11,7 +11,7 @@ int main() {
     const int alphabet_size = 26;
 
     int counts[alphabet_size] = {0};
-    const int offset = (int)'A';
+    const int offset = 'A';
     
     string s;
     cin >> s;
@@ -19,20 +19,10 @@ int main() {
     int n = s.size();
 
     for (auto c : s) {
-        counts[(int)c - offset] += 1;
+        counts[c - offset] += 1;
     }
 
     int odd_idx = -1;
-
-    for (int i = 0; i < alphabet_size; ++i) {
-        if (counts[i] % 2 != 0) {
-            if (odd_idx != -1) {
-                cout << "NO SOLUTION\n";
-                return 0;
-            }
-            odd_idx = i;
-        }
-    }
 
     char* palindrome = new char[n];
     int l_idx = 0;
@@ -40,17 +30,23 @@ int main() {
 
     for (int i = 0; i < alphabet_size; ++i) {
         int count = counts[i];
-        if (count > 0) {
-            char c = (char)(i + offset);
-            for (int j = 0; j < count / 2; ++j) {
-                palindrome[l_idx++] = c;
-                palindrome[r_idx--] = c;
+        if (count == 0) continue;
+        if (counts[i] % 2 != 0) {
+            if (odd_idx != -1) {
+                cout << "NO SOLUTION\n";
+                return 0;
             }
+            odd_idx = i;
+        }
+        char c = i + offset;
+        for (int j = 0; j < count / 2; ++j) {
+            palindrome[l_idx++] = c;
+            palindrome[r_idx--] = c;
         }
     }
 
     if (odd_idx != -1) {
-        palindrome[n / 2] = (char)(odd_idx + offset);
+        palindrome[n / 2] = odd_idx + offset;
     }
 
     cout << palindrome << '\n';
